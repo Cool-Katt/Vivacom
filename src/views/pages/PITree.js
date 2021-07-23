2f2c77f5a7af6ea8f73fccd5c7c5a7036e7c9fcb
Cool-Katt <69585372+Cool-Katt@users.noreply.github.com>
2021-07-23 11:39:20 +0300

    Added Page alert for query submit

--- src/views/pages/PITree.js
+++ src/views/pages/PITree.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Card, CardBody, CardFooter, Button, Row, Col, CardHeader, Form, FormGroup, Input} from 'reactstrap';
 import Tree from 'react-d3-tree';
 import TreeLegend from "../../vibe/components/PITree/TreeLegend";
+import PageAlertContext from "../../vibe/components/PageAlert/PageAlertContext";
 import downloadSvg, {downloadPng} from "svg-crowbar";
 import {Loader} from "../../vibe";
 
@@ -19,8 +20,7 @@ class PITree extends Component {
     }
 
     componentDidMount() {
-        if (sessionStorage.getItem('treeData'))
-        {
+        if (sessionStorage.getItem('treeData')) {
             document.querySelector('.submitButton').click()
         }
     }
@@ -64,40 +64,47 @@ class PITree extends Component {
         let submitButton = document.querySelector('.submitButton');
         if (buttonState === 'disabled') {
             submitButton.setAttribute('disabled', 'true');
-            this.setState(prevState => {return {...prevState, buttonDisabled: !prevState.buttonDisabled}});
+            this.setState(prevState => {
+                return {...prevState, buttonDisabled: !prevState.buttonDisabled}
+            });
         } else if (buttonState === 'enabled') {
             submitButton.removeAttribute('disabled');
-            this.setState(prevState => {return {...prevState, buttonDisabled: !prevState.buttonDisabled}});
+            this.setState(prevState => {
+                return {...prevState, buttonDisabled: !prevState.buttonDisabled}
+            });
         }
     };
 
-    handleSubmit(e) {
-        e.preventDefault();
+    handleSubmit(context) {
+        return (e => {
+            e.preventDefault();
 
-        let formData = new FormData(e.currentTarget);
-        let data = {
-            msisdn: formData.get('msisdn'),
-            dateStart: formData.get('date'),
-        }
-        this.toggleButton('disabled')
-        //TODO: Handle data requests to the API for tree. Talk to Stef
-        fetch('http://panoramamed/API_KQI_PI/userPi/monthly', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({...data}),
+            let formData = new FormData(e.currentTarget);
+            let data = {
+                msisdn: formData.get('msisdn'),
+                dateStart: formData.get('date'),
+            }
+            this.toggleButton('disabled')
+            //TODO: Handle data requests to the API for tree. Talk to Stef
+            fetch('http://panoramamed/API_KQI_PI/userPi/monthly', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({...data}),
+            })
+                .then(response => response.json())
+                .then(r => this.setState({treeData: JSON.parse(r)}))
+                .then(() => this.toggleButton('enabled'))
+                .catch(err => {
+                    console.log(err);
+                    this.setState({treeData: null});
+                    this.toggleButton('enabled');
+                });
+
+            context.setAlert('⚠️ Please wait for data to load. It shouldn\'t take more than a minute.', 'primary');
+            sessionStorage.setItem('treeData', JSON.stringify(data))
         })
-            .then(response => response.json())
-            .then(r => this.setState({treeData: JSON.parse(r)}))
-            .then(()=> this.toggleButton('enabled'))
-            .catch(err => {
-                console.log(err);
-                this.setState({treeData: null});
-                this.toggleButton('enabled');
-            });
-
-        sessionStorage.setItem('treeData', JSON.stringify(data))
     }
 
     render() {
@@ -107,44 +114,48 @@ class PITree extends Component {
                     <Col md={11} className='m-a-auto'>
                         <Card style={{height: '100%'}}>
                             <CardHeader>
-                                <Form onSubmit={this.handleSubmit}
-                                      style={{display: 'flex', alignContent: 'flex-start'}}>
-                                    <FormGroup className='p-t-md p-r-sm p-l'>
-                                        <Input type="number" name="msisdn" id="msisdn" placeholder="MSISDN"
-                                               defaultValue={JSON.parse(sessionStorage.getItem('treeData'))?.msisdn} required/>
-                                    </FormGroup>
-                                    <FormGroup className='p-t-md'>
-                                        <Input type="month" name="date" id="endDate"
-                                               max={this.state.thisMonth.toString()}
-                                               placeholder="20yy-MM"
-                                               defaultValue={JSON.parse(sessionStorage.getItem('treeData'))?.dateStart ?? this.state.thisMonth.toString()}
-                                               pattern="20[0-9]{2}-[0-1][0-9]" required/>
-                                    </FormGroup>
-                                    <FormGroup className='p-t-md'>
-                                        <Button className='submitButton' color='primary' outline>
-                                            {this.state.buttonDisabled ?
-                                                <Loader type='spin' small/> :
-                                                <i className='fa fa-search'/>
-                                            }
-                                        </Button>
-                                        <Button color='info' outline onClick={e => {
-                                            e.preventDefault();
-                                            let svg = document.querySelector('.rd3t-svg');
-                                            downloadSvg(svg, 'PITreeAsSVG', {css: 'internal'});
-                                        }}>
-                                            <i className='fa fa-save'/>
-                                            &nbsp;Save As SVG
-                                        </Button>
-                                        <Button color='success' outline onClick={e => {
-                                            e.preventDefault();
-                                            let svg = document.querySelector('.rd3t-svg');
-                                            downloadPng(svg, 'PITreeASPNG', {css: 'internal'});
-                                        }}>
-                                            <i className='fa fa-save'/>
-                                            &nbsp;Save As PNG
-                                        </Button>
-                                    </FormGroup>
-                                </Form>
+                                <PageAlertContext.Consumer>
+                                    {context => (
+                                        <Form onSubmit={this.handleSubmit(context)}
+                                              style={{display: 'flex', alignContent: 'flex-start'}}>
+                                            <FormGroup className='p-t-md p-r-sm p-l'>
+                                                <Input type="number" name="msisdn" id="msisdn" placeholder="MSISDN"
+                                                       defaultValue={JSON.parse(sessionStorage.getItem('treeData'))?.msisdn}
+                                                       required/>
+                                            </FormGroup>
+                                            <FormGroup className='p-t-md'>
+                                                <Input type="month" name="date" id="endDate"
+                                                       max={this.state.thisMonth.toString()}
+                                                       placeholder="20yy-MM"
+                                                       defaultValue={JSON.parse(sessionStorage.getItem('treeData'))?.dateStart ?? this.state.thisMonth.toString()}
+                                                       pattern="20[0-9]{2}-[0-1][0-9]" required/>
+                                            </FormGroup>
+                                            <FormGroup className='p-t-md'>
+                                                <Button className='submitButton' color='primary' outline>
+                                                    {this.state.buttonDisabled ?
+                                                        <Loader type='spin' small/> :
+                                                        <i className='fa fa-search'/>
+                                                    }
+                                                </Button>
+                                                <Button color='info' outline onClick={e => {
+                                                    e.preventDefault();
+                                                    let svg = document.querySelector('.rd3t-svg');
+                                                    downloadSvg(svg, 'PITreeAsSVG', {css: 'internal'});
+                                                }}>
+                                                    <i className='fa fa-save'/>
+                                                    &nbsp;Save As SVG
+                                                </Button>
+                                                <Button color='success' outline onClick={e => {
+                                                    e.preventDefault();
+                                                    let svg = document.querySelector('.rd3t-svg');
+                                                    downloadPng(svg, 'PITreeASPNG', {css: 'internal'});
+                                                }}>
+                                                    <i className='fa fa-save'/>
+                                                    &nbsp;Save As PNG
+                                                </Button>
+                                            </FormGroup>
+                                        </Form>)}
+                                </PageAlertContext.Consumer>
                             </CardHeader>
                             <CardBody className='capture-node'>
                                 {this.state.treeData ?
@@ -155,7 +166,8 @@ class PITree extends Component {
                                            depthFactor='400'
                                            enableLegacyTransitions={true}
                                            renderCustomNodeElement={this.renderSvgNode}/>)
-                                    : (<legend className='m-a-auto p-a-xxl'>Nothing to see here yet.<br />Go ahead and make a new query.</legend>)
+                                    : (<legend className='m-a-auto p-a-xxl'>Nothing to see here yet.<br/>Go ahead and
+                                        make a new query.</legend>)
                                 }
                             </CardBody>
                             <CardFooter>
